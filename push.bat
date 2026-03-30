c4c6537877db2a98da99d744d4f074657eba5ede
Shubham <babubhaiyaskater@gmail.com>
2026-03-30 09:51:59 +0530

    auto update

---
+++ push.bat
@@ -0,0 +1,12 @@
+@echo off
+echo =========================
+echo   AUTO GITHUB PUSH 🚀
+echo =========================
+
+git add .
+git commit -m "auto update"
+git push
+
+echo.
+echo DONE ✅
+pause
